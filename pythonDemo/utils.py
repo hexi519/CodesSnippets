64e3aa6f1c0d24f1581060ac33962d1444510020
hexi519 <244752705@qq.com>
2021-12-10 13:44:05 +0800

    chore

--- pythonDemo/utils.py
+++ pythonDemo/utils.py
@@ -4,8 +4,12 @@
 @Contact :   hesy519@gmail.com
 @Desc    :   [useful functions]
 '''
-# ====== 保存和读取数据文件 ======
 def save_obj(obj, name:str ):
+    """保存和读取数据文件
+    Args:
+        obj ([type]): 保存的对象
+        name (str): 保存的对象名字前缀 (name.pkl)
+    """
     import pickle
     with open( f"{name}.pkl", 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
@@ -15,15 +19,34 @@ def load_obj(name:str):
     with open(f"{name}.pkl", 'rb') as f:
         return pickle.load(f)
 
-# ====== 按某粒度平滑数据 ======
 def smooth_data( datas, numPerGroup:int ): # ->List
     """按某粒度平滑数据
     Args:
         datas : 需要被平滑的的数据
         numPerGroup : 每组数据的数量
-
     Returns:
         List: 分组后每组数据的均值
     """
     from more_itertools import chunked
     return chunked( sum(x)/len(x) ,numPerGroup )
+
+class safeContext:
+    """catch exceptgion with context
+    """
+    def __enter__(self):
+        return 1
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        print('exc_type: %s' % exc_type)
+        print('exc_value: %s' % exc_value)
+        print('exc_tb: %s' % exc_tb)
+
+def testSafeContext():
+    with safeContext() as cont:
+        test = open('./test','r')
+
+def main():
+    testSafeContext()
+
+if __name__=='__main__':
+    main()
